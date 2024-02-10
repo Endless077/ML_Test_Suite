2b0e06b7a88b149262e7a4c9c2481e3dba24c1bf
Antonio <antonio.garofalo125@gmail.com>
2024-02-11 00:38:23 +0100

    Fix some github bugs and update project structure

--- fastapi/handler.py
+++ fastapi/handler.py
@@ -1,32 +1,31 @@
 # Utils
-from scripts.utils.load_dataset import *
-from scripts.utils.load_model import *
-from scripts.utils.model import *
-from scripts.utils.utils import *
+from utils.load_dataset import *
+from utils.load_model import *
+from utils.model import *
+from utils.utils import *
 
 # Classes
-from scripts.utils.classes.ImageSerializer import ImageSerializer
-from scripts.utils.classes.AttackClass import *
-from scripts.utils.classes.DefenseClass import *
+from utils.classes.ImageSerializer import ImageSerializer
+from utils.classes.AttackClass import *
+from utils.classes.DefenseClass import *
 
 # ML_Attacks
-from scripts.utils.ml_attacks.evasion.FGM import FGM
-from scripts.utils.ml_attacks.evasion.PGD import PGD
-from scripts.utils.ml_attacks.extraction.CopycatCNN import CopycatCNN
-from scripts.utils.ml_attacks.inference.MIFace import MIFace
-from scripts.utils.ml_attacks.poisoning.CleanLabelBackdoor import CleanLabelBackdoor
-from scripts.utils.ml_attacks.poisoning.SimpleBackdoor import SimpleBackdoor
+from utils.ml_attacks.evasion.FGM import FGM
+from utils.ml_attacks.evasion.PGD import PGD
+from utils.ml_attacks.extraction.CopycatCNN import CopycatCNN
+from utils.ml_attacks.inference.MIFace import MIFace
+from utils.ml_attacks.poisoning.CleanLabelBackdoor import CleanLabelBackdoor
+from utils.ml_attacks.poisoning.SimpleBackdoor import SimpleBackdoor
 
 # ML_Defenses
-from scripts.utils.ml_defenses.detector.ActivationDefense import ActivationDefense
-from scripts.utils.ml_defenses.postprocessor.ReverseSigmoid import ReverseSigmoid
-from scripts.utils.ml_defenses.preprocessor.TotalVarMin import TotalVarMin
-from scripts.utils.ml_defenses.trainer.AdversarialTrainer import AdversarialTrainer
-from scripts.utils.ml_defenses.transformer.STRongIntentionalPerturbation import STRongIntentionalPerturbation
+from utils.ml_defenses.detector.ActivationDefense import ActivationDefense
+from utils.ml_defenses.postprocessor.ReverseSigmoid import ReverseSigmoid
+from utils.ml_defenses.preprocessor.TotalVarMin import TotalVarMin
+from utils.ml_defenses.trainer.AdversarialTrainer import AdversarialTrainer
+from utils.ml_defenses.transformer.STRongIntentionalPerturbation import STRongIntentionalPerturbation
 
 ###################################################################################################
 
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import uvicorn
+from payload import *
+
+###################################################################################################
